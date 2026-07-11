a72d637d6190eb50eb696a8fcac2a7a58b219f52
1saqibpathan1 <90570509+1saqibpathan1@users.noreply.github.com>
2026-07-12 03:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.secret_key = "change_this_secret"
 
 VIDEO_MAP_FILE = "video_id_map.json"
 DATABASE_URL = os.environ.get("DATABASE_URL")
+print("DATABASE_URL =", DATABASE_URL)
+print("DATABASE_URL exists:", DATABASE_URL is not None)
 CLEAN_TRANSCRIPTS_FOLDER = "claude_cleaned_50"
 
 
